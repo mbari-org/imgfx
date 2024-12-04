b4c37c7827558993a386b3ee9b923d254b79cc35
Brian Schlining <bschlining@gmail.com>
2024-12-04 13:40:30 -0800

    prepping for deployment to maven central

--- src/main/java/org/mbari/imgfx/roi/MarkerBuilder.java
+++ src/main/java/org/mbari/imgfx/roi/MarkerBuilder.java
@@ -4,8 +4,8 @@ import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import org.mbari.imgfx.AutoscalePaneController;
 import org.mbari.imgfx.Builder;
-import org.mbari.imgfx.etc.rx.events.AddMarkerEvent;
 import org.mbari.imgfx.etc.rx.EventBus;
+import org.mbari.imgfx.etc.rx.events.AddMarkerEvent;
 
 public class MarkerBuilder implements Builder<MarkerView> {
 
@@ -14,27 +14,35 @@ public class MarkerBuilder implements Builder<MarkerView> {
     private final EventBus eventBus;
     private double radius = 6D;
 
-    public MarkerBuilder(AutoscalePaneController<?> paneController, EventBus eventBus) {
+    public MarkerBuilder(
+        AutoscalePaneController<?> paneController,
+        EventBus eventBus
+    ) {
         this.paneController = paneController;
         this.eventBus = eventBus;
         init();
     }
 
     private void init() {
-        paneController.getPane().setOnMouseClicked(event -> {
-            if (!disabled.get()) {
-                MarkerView.fromSceneCoords(event.getSceneX(), event.getSceneY(), radius, paneController.getAutoscale())
-                        .ifPresent(view -> {
-                            // Set the radius in the image data so the circle scales correctly with
-                            // image resize
-                            view.getData().setRadius(radius);
-                            var loc = new Localization<>(view, paneController);
-                            eventBus.publish(new AddMarkerEvent(loc));
-                        });
-            }
-        });
+        paneController
+            .getPane()
+            .setOnMouseClicked(event -> {
+                if (!disabled.get()) {
+                    MarkerView.fromSceneCoords(
+                        event.getSceneX(),
+                        event.getSceneY(),
+                        radius,
+                        paneController.getAutoscale()
+                    ).ifPresent(view -> {
+                        // Set the radius in the image data so the circle scales correctly with
+                        // image resize
+                        view.getData().setRadius(radius);
+                        var loc = new Localization<>(view, paneController);
+                        eventBus.publish(new AddMarkerEvent<>(loc));
+                    });
+                }
+            });
     }
-
 
     public double getRadius() {
         return radius;
